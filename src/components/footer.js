3da941a84040dfedfb60a4704eb68f3e758dcc7f
Joshua Patterson <joshualpatterson@gmail.com>
2019-06-10 16:56:01 -0400

    Fixed up footer style and anchor hrefs

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
+import ui from '../styles/global-style-variables'
 
 const Container = styled.footer`
     flex-shrink: 0;
-    background: #0094FF;
-    color: #F2F2F2;
+    background: ${ui.globalStyles.blue};
+    color: ${ui.globalStyles.offWhite};
     padding: 5px;
     text-align: center;
+
+    & p span {
+        margin: 0 10px;
+
+        a {
+            color: ${ui.globalStyles.offWhite};
+            transition: color .25s ease-out;
+        }
+        a:focus {
+            color: ${ui.globalStyles.lightOrange};
+            outline: none;
+        }
+        a:hover {
+            color: ${ui.globalStyles.orange};
+        }
+    }
 `
 
 const Footer = () => {
     return (
         <Container>
-            <span>SomethingToCook.com &copy;</span>
-            |
-            <span>Explore the <a href="https://google.com">STC API</a></span>
-            |
-            <span>View on <a href="https://google.com">Github</a></span>
+            <p>
+                <span>SomethingToCook.com &copy;</span>
+                | 
+                <span>Explore the <a href='https://github.com/shnibble/stc-api' target='_BLANK'>STC API</a></span>
+                | 
+                <span>View on <a href='https://github.com/shnibble/stc-app' target='_BLANK'>Github</a></span>
+            </p>
         </Container>
     )
 }
